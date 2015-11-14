b770712f3358efbe32e903b85a2241f39376b27d
bdadam <bdadam@mail.bradley.edu>
2015-11-14 03:20:34 -0600

    fixed spelling error in submit

--- submit.php
+++ submit.php
@@ -5,7 +5,7 @@
 		file_put_contents("msg/$filename", $_POST['msg']);
 		$msgs = glob('msg/*.txt');
 		foreach ($msgs as $filename) {
-			$msg = file_get_contnets($filename);
+			$msg = file_get_contents($filename);
 			echo '<p>' . htmlentities($msg) . '</p>';
 		}
 	}
