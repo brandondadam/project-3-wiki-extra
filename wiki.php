9f76bd7266866d8f0f016a3b3cf8168232a4b514
bdadam <bdadam@mail.bradley.edu>
2015-11-14 02:52:13 -0600

    fixed error

--- wiki.php
+++ wiki.php
@@ -14,7 +14,7 @@
 		<div id="msgs">
 			<?php
 			$msgs = glob('msg/*.txt');
-			foreach ($msgs as filename) {
+			foreach ($msgs as $filename) {
 				$msg = file_get_contnets($filename);
 				echo '<p>'.htmlentities($msg).'</p>';
 			}
