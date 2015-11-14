a257083efadd5a6e6b17e0f2955f1a127110a60d
bdadam <bdadam@mail.bradley.edu>
2015-11-14 04:00:34 -0600

    this should work

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width">
 		<title>Wiki</title>
-		<link rel="stylesheet" href="wiki.css">
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
 		<h1>
@@ -15,17 +15,17 @@
 			<?php
 			$msgs = glob('msg/*.txt');
 			foreach ($msgs as $filename) {
-				$msg = file_get_contnets($filename);
+				$msg = file_get_contents($filename);
 				echo '<p>'.htmlentities($msg).'</p>';
 			}
 			?>
 		</div>
-		<form action="submit.php" id="type" method="post">
+		<form action="submit.php" method="post">
 			<input type="text" name="msg" placeholder="Type your message here." autocomplete="off">
 			<input type="submit" value="Send">
 		</form>
 	</div>
 	<script src="jquery-1.11.3.min.js"></script>
-	<script src="wiki.js"></script>
+	<script src="script.js"></script>
 	</body>
 </html>
