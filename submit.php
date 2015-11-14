d04a99b75b3ad3ba9495b8487ff2b28415877883
bdadam <bdadam@mail.bradley.edu>
2015-11-14 03:01:30 -0600

    fixed another error

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
 	if(!empty($_POST['msg'])){
 		$filename = microtime(true);
 		$filename = str_replace('.', '_', $filename) . '.txt';
-		file_put_contents("msg/filename", $_POST['msg']);
+		file_put_contents("msg/$filename", $_POST['msg']);
 		$msgs = glob('msg/*.txt');
 		foreach ($msgs as $filename) {
 			$msg = file_get_contnets($filename);
