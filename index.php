79a3fb7d29ec23b3973ecd081d7db0f17d5065b9
bdadam <bdadam@mail.bradley.edu>
2015-11-16 14:42:38 -0600

    this should work well with input

--- index.php
+++ index.php
@@ -4,29 +4,28 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width">
 		<title>Wiki</title>
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="wiki.css">
 	</head>
 	<body>
 		<h1>
 			The coolest wiki ever!
 		</h1>
 	<div id="page">
-		<form action="submit.php" id="type" onsubmit="return false;" onKeyDown="return addText(event);" method="post">
-			<input type="text" name="msg" placeholder="Type your message here." autocomplete="off"/>
-			<input type="submit" id="submit" value="Send">
-			<input />
-		</form>
 		<div id="msgs">
 			<?php
 			$msgs = glob('msg/*.txt');
 			foreach ($msgs as $filename) {
-				$msg = file_get_contents($filename);
+				$msg = file_get_contnets($filename);
 				echo '<p>'.htmlentities($msg).'</p>';
 			}
 			?>
 		</div>
+		<form action="submit.php" id="type" method="post">
+			<input type="text" name="msg" placeholder="Type your message here." autocomplete="off">
+			<input type="submit" value="Send">
+		</form>
 	</div>
 	<script src="jquery-1.11.3.min.js"></script>
-	<script src="script.js"></script>
+	<script src="wiki.js"></script>
 	</body>
 </html>
