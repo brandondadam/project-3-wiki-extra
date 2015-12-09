257c2b97a13a9090e63c4f135625bd247f4e05c0
bdadam <bdadam@mail.bradley.edu>
2015-12-09 02:00:28 -0600

    added working files for a basic wiki

--- index.php
+++ index.php
@@ -3,28 +3,28 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width">
-		<title>Wiki</title>
+		<title>Story Builder</title>
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-		<h1>
-			The coolest wiki ever!
-		</h1>
-	<div id="page">
-		<form action="submit.php" id="type" method="post">
-			<input type="text" name="msg" placeholder="Type your message here." autocomplete="off">
+		<form action="submit.php" method="post">
+			<textarea type="text" id="type" name="msg" placeholder="Add to the story..." rows="8" cols"80"></textarea>
+			<input type="submit" value="Send">
 		</form>
+
 		<div id="msgs">
-			<?php
-			$msgs = glob('msg/*.txt');
-			foreach ($msgs as $filename) {
-				$msg = file_get_contents($filename);
-				echo '<p>'.htmlentities($msg).'</p>';
-			}
-			?>
+				<?php
+					if (!file_exists('msg')) {
+						mkdir('msg');
+					}
+					$msgs=glob('msg/*.txt');
+					foreach ($msgs as $filename){
+						$msg = file_get_contents($filename);
+						echo '<p>' . htmlentities($msg) . '</p>';
+					}
+				?>
 		</div>
-	</div>
-	<script src="jquery-1.11.3.min.js"></script>
-	<script src="script.js"></script>
+		<script src="jquery-1.11.3.min.js"></script>
+		<script src="script.js"></script>
 	</body>
 </html>
--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
-	$msgs = glob('msg/*.txt');
-		foreach ($msgs as $filename) {
-			$msg = file_get_contents($filename);
-			echo '<p>' . htmlentities($msg) . '</p>';
-		}
+	$msgs=glob('msg/*.txt');
+	foreach ($msgs as $filename){
+		$msg = file_get_contents($filename);
+		echo '<p>' . htmlentities($msg) . '</p>';
+	}
 ?>
